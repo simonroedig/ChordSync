1b568ec5817bafd9788c1b1f80ddeb20889c0b12
simonroedig <simon.roedig@web.de>
2023-12-09 17:54:58 +0100

    idk help

--- app.py
+++ app.py
@@ -1200,25 +1200,3 @@ atexit.register(cleanup)
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
 """ 
-    
-def run(host='0.0.0.0', port=5000, workers=1 + (multiprocessing.cpu_count() * 2)):
-    """Run the app with Gunicorn."""
-
-    if app.debug:
-        app.run(host, int(port), use_reloader=False)
-    else:
-        gunicorn = WSGIApplication()
-        gunicorn.load_wsgiapp = lambda: app
-        gunicorn.cfg.set('bind', '%s:%s' % (host, port))
-        gunicorn.cfg.set('workers', workers)
-        gunicorn.cfg.set('threads', workers)
-        gunicorn.cfg.set('pidfile', None)
-        gunicorn.cfg.set('worker_class', 'sync')
-        gunicorn.cfg.set('keepalive', 10)
-        gunicorn.cfg.set('accesslog', '-')
-        gunicorn.cfg.set('errorlog', '-')
-        gunicorn.cfg.set('reload', True)
-        gunicorn.chdir()
-        gunicorn.run()
-
-
